fbff9accdc3bdfa401aa2ac86502bcfa04133c29
llamaHs <h29mays@gmail.com>
2026-02-28 23:04:45 +0900

    fix: reset scroll position on route change

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
 import FAQ from "../components/FAQ";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 function AppLayout({ startLoading, finishLoading }) {
   return (
     <>
+      <ScrollToTop />
       <Nav startLoading={startLoading} />
       <Outlet context={{ startLoading, finishLoading }} />
       <FAQ />
